5f331cf7085644dbc48702055b61e21e930754a0
ozdencodes <ozdencodes@users.noreply.github.com>
2021-02-28 14:58:16 +0300

    about-me

---
+++ Script/main.js
@@ -0,0 +1,7 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
