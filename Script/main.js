cb5260b916b4b6521418e02659741ffdbe59e070
ozdencodes <ozdencodes@users.noreply.github.com>
2021-11-26 01:31:25 +0300

    Update main.js

--- Script/main.js
+++ Script/main.js
@@ -1,7 +1,3 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
+$(document).ready(function() {
+  console.log( "ready!" );
+});
